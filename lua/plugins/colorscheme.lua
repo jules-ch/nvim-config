746f23a72bbacf2fcfd05e720f44f02c281193bc
Jules Chéron <jules.cheron@gmail.com>
2025-10-29 21:40:29 +0100

    chore: remove debug and catpuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -23,9 +23,4 @@ return {
       vim.cmd [[colorscheme tokyonight-night]]
     end,
   },
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
-  },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -296,7 +296,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'debugpy',
+        -- 'debugpy',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
